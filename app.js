8f274bfdc780793cf4917444ebc2d4c57cd61009
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-04-04 05:22:12 -0400

    created contact model

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// database setup
+let mongoose = require('mongoose');
+let DB = require('./config/db');
+
+// point Mongoose to the DB URI
+mongoose.connect(DB.URI);
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', ()=> {
+  console.log("Connected to MongoDB...");
+});
+
 let indexRouter = require('./routes/index');
 let aboutRouter=require('./routes/about');
 let contactRouter=require('./routes/contact');
@@ -26,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/',aboutRouter);
-app.use('/',contactRouter);
+app.use('/contact-list',contactRouter);
 app.use('/',projectRouter);
 app.use('/',servicesRouter);
 
--- routes/contact.js
+++ routes/contact.js
@@ -6,9 +6,27 @@ Feb 15 2019*/
 let express = require("express");
 let router = express.Router();
 
-/* GET contact page. */
-router.get("/contact", function(req, res, next) {
-  res.render("contact", { title: "Contact" });
+let mongoose = require('mongoose');
+
+// create a reference to the db schema
+let contact = require('../models/contact');
+
+/* GET Contact List page - READ Operation */
+router.get('/', (req, res, next) =>{
+    contact.find((err, contactList) => {
+        if(err) {
+            return console.error(err);
+        }
+        else {
+            console.log(contactList);
+
+            res.render('contacts/index', {
+                title: 'Contact List',
+                contactList: contactList
+            });
+            
+        }
+    });
 });
 
 module.exports = router;
