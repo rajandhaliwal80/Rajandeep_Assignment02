1c05567758a4c6afc1119c97aacac40fd84dc70b
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-04-05 21:24:07 -0400

    added Update functionality

--- routes/contacts.js
+++ routes/contacts.js
@@ -54,4 +54,50 @@ let newContact = contactModel({
     });
 });
 
+/* GET Contact List page - READ Operation */
+router.get('/edit/:id', (req, res, next) =>{
+
+    let id = req.params.id;
+
+    console.log(id);
+    contactModel.findById(id, (err, contactObject) => {
+        if(err) {
+             console.log(err);
+             res.end(err);
+        }
+        else {
+            console.log(contactObject);
+
+            res.render('contacts/edit', {
+                title: 'Edit Contact',
+                contact: contactObject
+            });
+            
+        }
+    });
+});
+
+router.post('/edit/:id', (req,res,next)=> {
+    let id = req.params.id;
+    let updatedContact = contactModel({
+        "_id":id,
+        "firstName":req.body.firstName,
+        "lastName": req.body.lastName,
+        "age":req.body.age
+    })
+
+    contactModel.update({_id: id}, updatedContact, (err) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+       }
+       else {
+         
+
+           res.redirect('/contact-list');
+           
+       }
+    })
+});
+
 module.exports = router;
